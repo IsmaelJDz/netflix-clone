692c29d0fd6252781838301c259f2eec63e09c9e
Ismael R <ismael_br7@hotmail.com>
2023-05-05 01:41:11 -0600

    [NETFLIX: page]: landing page and header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import Navbar from '@/components/Navbar';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import { NextPageContext } from 'next';
-import { getSession, signOut } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,17 +21,37 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
   return (
     <>
-      <h1 className='text-2xl text-green-500'>Netflix clone</h1>
-      <p className='text-white'>Logged as: {user?.name} </p>
-      <button
-        className='w-full h-10 bg-white'
-        onClick={() => signOut()}>
-        Logout
-      </button>
+      <Navbar />
+      {/* <div className='bg-gray-500'>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+        <div className='h-96'></div>
+      </div> */}
     </>
   );
 }
